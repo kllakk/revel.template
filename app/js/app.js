423e9e15cba677c464a5754c2d23452236c5a188
BPROG2\Сергей <petrovsa83@mail.ru>
2017-02-06 18:42:25 +0300

    npm i --save-dev lodash и еще пример

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,16 @@
 import bar from './bar';
 
 bar();
+
+var _ = require('lodash');
+
+function component () {
+    var element = document.createElement('div');
+
+    /* lodash is required for the next line to work */
+    element.innerHTML = _.join(['Hello','webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
