4284dbb0bcdba750d8b5a4759bcfb11660b5abe9
BPROG2\Сергей <petrovsa83@mail.ru>
2017-02-06 18:48:37 +0300

    npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+var path = require('path');
+
 module.exports = {
     entry: './app/js/app.js',
     output: {
-        filename: './public/js/bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public', 'js')
     }
 }
