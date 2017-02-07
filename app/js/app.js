1bc8f5e6ec5d8547216d88892f4c262a2ad18ae6
BPROG2\Сергей <petrovsa83@mail.ru>
2017-02-07 12:07:41 +0300

    с  tether проблема не решена, но пока так через head работает

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,13 @@
-import bar from './bar';
-
-bar();
-
+import "jquery";
+import "tether";
+import "bootstrap";
+import "font-awesome/scss/font-awesome.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "./../css/site.scss";
 import _ from 'lodash';
+
+import main from './main';
+main();
 
 function component () {
     var element = document.createElement('div');
@@ -14,3 +19,22 @@ function component () {
 }
 
 document.body.appendChild(component());
+
+
+var $sidebar = 0;
+$('#toggleSidebar').click(function() {
+    if ($sidebar === 1) {
+        $('#sidebar').hide();
+        $('#toggleSidebar i').addClass('fa-chevron-left');
+        $('#toggleSidebar i').removeClass('fa-chevron-right');
+        $sidebar = 0;
+    }
+    else {
+        $('#sidebar').show();
+        $('#toggleSidebar i').addClass('fa-chevron-right');
+        $('#toggleSidebar i').removeClass('fa-chevron-left');
+        $sidebar = 1;
+    }
+
+    return false;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack'); //to access built-in plugins
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 
 const config = {
@@ -10,11 +11,38 @@ const config = {
     module: {
         rules: [
             {
+                test: /\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!sass-loader'
+                })
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
                 exclude: /(node_modules|bower_components)/,
-                test: /\.(js|jsx)$/, use: 'babel-loader'
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(woff2|woff|svg|ttf|eot)([\?]?.*)$/,
+                use: 'file-loader?name=[name].[ext]&publicPath=/public/fonts/&outputPath=./../fonts/',
             }
         ]
-    }
+    },
+    plugins: [
+        //new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin('./../css/bundle.css'),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 }
 
 module.exports = config;
